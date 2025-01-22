f20153e7c51e739eb5ddac7eabb6e7508c522179
Moshe Shamouilian <Moshesham@gmail.com>
2025-01-22 22:57:17 +0200

    remving libs

--- streamlit_app/Product_Analytics/3_descriptive_statistics.py
+++ streamlit_app/Product_Analytics/3_descriptive_statistics.py
@@ -2,8 +2,9 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from scipy import stats
-import seaborn as sns
+
+# from scipy import stats
+# import seaborn as sns
 import matplotlib.pyplot as plt
 from utils import data_utils, stats_utils, viz_utils, style_utils  # Import utility functions
 
